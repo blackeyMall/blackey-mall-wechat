e595130561d75aedf9f8729f97159619022c13b0
Anywere <17621153820@163.com>
2018-12-17 21:28:15 +0800

    【note】金融圈 - 名片 & 信息 & 项目 页面更新

---
+++ Financial/pages/cardEdit/cardEdit.js
@@ -0,0 +1,79 @@
+// pages/cardEdit/cardEdit.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        cardInfo: {
+            avatarUrl: '../../assets/img/avatar.jpg',
+            name: '李小柒',
+            position: '',
+            tel: '',
+            companyFullName: '',
+            companyName: '',
+            companyUrl: '',
+            companyAddress: '',
+            wechat: '',
+            email: '',
+            // tags: ['股权投资', '债权投资', '股权投资', '融资', '股权投资', '融资']
+            tags: []
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
